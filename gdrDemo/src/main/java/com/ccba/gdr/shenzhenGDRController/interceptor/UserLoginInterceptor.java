c6b759fbead76513dab1d503798d4aab0dff785d
seven <bowenzhangas@163.com>
2024-11-10 15:52:18 +0800

    深圳开发中心提交

--- gdrDemo/src/main/java/com/ccba/gdr/shenzhenGDRController/interceptor/UserLoginInterceptor.java
+++ gdrDemo/src/main/java/com/ccba/gdr/shenzhenGDRController/interceptor/UserLoginInterceptor.java
@@ -2,4 +2,6 @@ package com.ccba.gdr.shenzhenGDRController.interceptor;
 
 public class UserLoginInterceptor {
     private static ThreadLocal currentUser = new ThreadLocal();
+
+    private static final String ERROR_CODE = "001";
 }
